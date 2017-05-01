7b489ed82713454ef768cff53072e004cf0b4a74
Subir <subir.chowdhuri@gmail.com>
2017-05-01 12:52:44 +0530

    Build config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
 const path = require("path");
-const webpack = require("webpack")
+const webpack = require("webpack");
 
-module.exports = {
-    entry: "./app/index.js",
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }]
-    },
-    output: {
-        filename: "chess.min.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    // plugins: [
-    //     new webpack.optimize.UglifyJsPlugin()
-    // ]
-};
+module.exports = function (env) {
+    const dev = Boolean(env && env.dev);
+    const watch = Boolean(env && env.watch);
+    const plugins = [];
+    let filename;
+    if(dev) {
+        filename = "chess.js";
+    } else {
+        plugins.push(new webpack.optimize.UglifyJsPlugin());
+        filename = "chess.min.js"
+    }
+
+    return {    
+       entry: "./app/index.js",
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }]
+        },
+        output: {
+            filename: filename,
+            path: path.resolve(__dirname, "dist")
+        },
+        plugins: plugins,
+        watch: watch
+    };
+}
